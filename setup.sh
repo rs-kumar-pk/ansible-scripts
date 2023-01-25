644db2bcb429d3ae021310f2aa08d26f9a200d36
nerellasravan <123354372+nerellasravan@users.noreply.github.com>
2023-01-25 10:59:57 -0500

    update

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 		sudo apt install ansible -y
 		sudo apt-get install zip unzip
 		sudo apt install net-tools
-		sudo git clone https://github.com/rs-pk/ansiblesonar.git
+		sudo git clone https://github.com/rs-kumar-pk/ansiblesonar.git
 		sudo ansible-playbook ansiblesonar/ansible_config/site.yml
