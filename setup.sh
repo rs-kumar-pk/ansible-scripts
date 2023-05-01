f12dbac0cd501a1ae80712cba1ddee721cdb77cb
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-05-01 12:35:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 		sudo apt-get install zip unzip
 		sudo apt install net-tools
 		sudo git clone https://github.com/rs-kumar-pk/ansible-scripts.git
-		sudo ansible-playbook ansible-scripts/ansible_config/site.yml
+		sudo ansible-playbook ansible-scripts/ansible_config/site-docker.yml
